3ac14b7ced240fce9a347b7d6db72a68149223cc
cold <kholodihor@gmail.com>
2024-01-02 11:05:41 +0200

    update code

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -1,12 +1,12 @@
-import CommentModel from '../models/CommentModel.js';
-import PostModel from '../models/PostModel.js';
+import CommentModel from "../models/CommentModel.js";
+import PostModel from "../models/PostModel.js";
 
 export const createComment = async (req, res) => {
   try {
     const { userId, comment } = req.body;
     const postId = req.params.id;
 
-    if (!comment) return res.json({ message: 'Comment can`t be empty' });
+    if (!comment) return res.json({ message: "Comment can`t be empty" });
 
     const newComment = new CommentModel({ comment, user: userId });
     await newComment.save();
@@ -21,7 +21,7 @@ export const createComment = async (req, res) => {
     res.json(newComment);
   } catch (error) {
     res.status(500).json({
-      message: 'Fail to create a comment',
+      message: "Fail to create a comment",
     });
   }
 };
